5fcebe88972b3f8633c1108900d9cc5993b2bb10
vshal-ch <chirumamilla.vishal2019@vitstudent.ac.in>
2021-11-13 20:18:13 +0530

    completed design part

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,13 @@ module.exports = {
     extend: {
       colors: {
         "strong-cyan": "hsl(172,67%,45%)",
+        "light-strong-cyan": "hsl(172,87%,45%)",
         "very-dark-cyan": "hsl(183,100%,15%)",
         "dark-grayish-cyan": "hsl(184,14%,43%)",
         "dark-grayish-cyan-2": "hsl(184,14%,56%)",
         "light-grayish-cyan": "hsl(184,14%,84%)",
         "light-grayish-cyan-2": "hsl(184,14%,97%)",
+        "red-cyan":"hsl(20,67%,45%)"
       },
       fontFamily: {
         display: ['"Space Mono"', "Helvetica", "Arial"],
